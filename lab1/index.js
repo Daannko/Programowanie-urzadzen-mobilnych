1d758b4abc370b9c740fcfc1307b4631e1e9fd25
student <student@L404-15.helium.com>
2022-10-13 17:03:33 +0200

    added two functions

--- lab1/index.js
+++ lab1/index.js
@@ -1,27 +1,39 @@
-var c = document.getElementById("myCanvas");
-var ctx = c.getContext("2d");
 
 
-var i = 1;
-function change() {
 
+
+var i = 0;
+var i2 = 1;
+function changeFirst() {
+
+
+    var c = document.getElementById("myCanvas");
+    var ctx = c.getContext("2d");
     ctx.beginPath();
 
-    
-    var grad = ctx.createLinearGradient(0,0,i,0);
+    i = !i;
 
-
-    grad.addColorStop(0,"blue");
-    grad.addColorStop(1,"red");
-
-    ctx.fillStyle = grad;
-
-    ctx.arc(400, 300, 100, 0, 2 * Math.PI);
-    ctx.arc(100, 300, 100, 0, 2 * Math.PI);
-    
-
+    ctx.fillStyle = `rgb(0,${i * 255},${(1 - i) * 255})`;
+    ctx.arc(400, 300, 50, 0, 2 * Math.PI);
     ctx.fill();
 
 }
 
-setInterval(change, 100);
+function changeSecond() {
+
+    var c = document.getElementById("myCanvas");
+    var ctx = c.getContext("2d");
+    ctx.beginPath();
+
+    i2 = (i2 + 1) % 255
+
+    ctx.fillStyle = `rgb(0,${i2},${255 - i2})`;
+    ctx.arc(200, 300, 50, 0, 2 * Math.PI);
+    ctx.fill();
+
+
+
+}
+
+setInterval(changeSecond, 10);
+setInterval(changeFirst, 1000);
