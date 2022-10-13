365fc1cc2275d5253b0c5232453a34cc0f5e3485
student <student@L404-15.helium.com>
2022-10-13 17:23:55 +0200

    third circle bounces

--- lab1/index.js
+++ lab1/index.js
@@ -4,9 +4,9 @@
 
 var i = 0;
 var i2 = 1;
+var i3 = 0;
+var flag = 1;
 function changeFirst() {
-
-
     var c = document.getElementById("myCanvas");
     var ctx = c.getContext("2d");
     ctx.beginPath();
@@ -16,7 +16,6 @@ function changeFirst() {
     ctx.fillStyle = `rgb(0,${i * 255},${(1 - i) * 255})`;
     ctx.arc(400, 300, 50, 0, 2 * Math.PI);
     ctx.fill();
-
 }
 
 function changeSecond() {
@@ -28,12 +27,37 @@ function changeSecond() {
     i2 = (i2 + 1) % 255
 
     ctx.fillStyle = `rgb(0,${i2},${255 - i2})`;
-    ctx.arc(200, 300, 50, 0, 2 * Math.PI);
+    ctx.arc(250, 300, 50, 0, 2 * Math.PI);
     ctx.fill();
+}
 
+function changeThird() {
 
+    var c = document.getElementById("myCanvas");
+    var ctx = c.getContext("2d");
+    ctx.beginPath();
 
+    
+    ctx.clearRect(0, 0, 200, 800);
+
+   
+    if(flag){
+        i3 = i3 + 50;
+    }
+    else{
+        i3 = i3 - 50;
+    }
+
+    
+    if(i3 == 500 || i3 == 0) {
+        flag = !flag
+    }
+    ctx.fillStyle = `rgb(0,${i2},${255 - i2})`;
+    ctx.arc(100,  50 +  i3, 50, 0, 2 * Math.PI);
+    ctx.fill();
+    
 }
 
 setInterval(changeSecond, 10);
 setInterval(changeFirst, 1000);
+setInterval(changeThird, 100);
