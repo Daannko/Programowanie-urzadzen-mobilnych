4e7b2e0b2e5c3ca455ed0425db52f7af84b4c85d
Daannko <228776@edu.p.lodz.pl>
2022-10-20 17:05:17 +0200

    add stairs that show after time

---
+++ d
@@ -0,0 +1,65 @@
+[33mcommit 431265968a6afa0f4a3788884a9f16fe2f099a87[m[33m ([m[1;36mHEAD -> [m[1;32mmaster[m[33m)[m
+Author: Daniel <daniel.kozarski@gmail.com>
+Date:   Thu Oct 20 16:27:35 2022 +0200
+
+    added folder lab2
+
+[33mcommit 45f651b9b64f6dd2d324b5305c703fae345cd247[m[33m ([m[1;31morigin/master[m[33m, [m[1;31morigin/HEAD[m[33m)[m
+Author: student <student@L404-15.helium.com>
+Date:   Thu Oct 13 17:34:47 2022 +0200
+
+    repaired small bugs
+
+[33mcommit 19aad1f41b873974ff4b5c671e5351da542b3121[m
+Author: student <student@L404-15.helium.com>
+Date:   Thu Oct 13 17:33:30 2022 +0200
+
+    repaired small bugs
+
+[33mcommit 205fde660f2d473556636182616063b6f1234b1b[m
+Author: student <student@L404-15.helium.com>
+Date:   Thu Oct 13 17:32:30 2022 +0200
+
+    repaired small bugs
+
+[33mcommit 365fc1cc2275d5253b0c5232453a34cc0f5e3485[m
+Author: student <student@L404-15.helium.com>
+Date:   Thu Oct 13 17:23:55 2022 +0200
+
+    third circle bounces
+
+[33mcommit 1d758b4abc370b9c740fcfc1307b4631e1e9fd25[m
+Author: student <student@L404-15.helium.com>
+Date:   Thu Oct 13 17:03:33 2022 +0200
+
+    added two functions
+
+[33mcommit 0100f9a73717a86d24de7f408ff88d6847f2e260[m
+Author: student <student@L404-15.helium.com>
+Date:   Thu Oct 13 16:40:32 2022 +0200
+
+    added lab1b folder
+
+[33mcommit a8c61cd90c2336ff29ac6c71ccd115cd3cd1c3bf[m
+Author: student <student@L404-15.helium.com>
+Date:   Thu Oct 13 16:37:16 2022 +0200
+
+    added 2nd circle
+
+[33mcommit 264d05cad402c234e023e3d66a47e85fad88f674[m
+Author: student <student@L404-15.helium.com>
+Date:   Thu Oct 6 17:35:44 2022 +0200
+
+    added circle that changes color
+
+[33mcommit f4bf30dd6ee4125a63b79e11a90e9f11f1154b6e[m
+Author: student <student@L404-15.helium.com>
+Date:   Thu Oct 6 17:14:11 2022 +0200
+
+    second commit
+
+[33mcommit 00313cfca9aeb7cde43d8f49fe25aef4849d22a9[m
+Author: student <student@L404-15.helium.com>
+Date:   Thu Oct 6 16:59:09 2022 +0200
+
+    first commit
--- lab2/index.js
+++ lab2/index.js
@@ -1,68 +1,43 @@
 
-var i = 0;
-var i2 = 1;
-var i3 = 0;
-var flag = 1;
-var flag2 = 0
-function changeFirst() {
-    var c = document.getElementById("myCanvas");
-    var ctx = c.getContext("2d");
+var c = document.getElementById("myCanvas");
+c.style.border = "1px solid black";
+var ctx = c.getContext("2d");
+var timer = 0;
+
+
+setInterval(drawS, 1000);
+
+function drawS(){
+
     ctx.beginPath();
 
-    i = !i;
+    if(timer >= 0){
+     ctx.rect(0,100,100,c.height - 100);
+    }
+    if(timer >= 1){
+        ctx.rect(100,200,100,c.height - 200);
+    }
+    if(timer >= 2){
+        ctx.rect(200,300,100,c.height - 300);
+    }
+    if(timer >= 3){
+        ctx.rect(300,400,100,c.height - 400);
+    }
+    if(timer >= 4){
+        ctx.rect(400,500,100,c.height - 500);
+    }
 
-    ctx.fillStyle = `rgb(0,${i * 255},${(1 - i) * 255})`;
-    ctx.arc(400, 300, 50, 0, 2 * Math.PI);
-    ctx.fill();
+    ctx.strokeStyle = "black";
+    ctx.stroke();
+    timer += 1;
+    console.log(timer);
 }
 
-function changeSecond() {
+function drawB(){
 
-    var c = document.getElementById("myCanvas");
-    var ctx = c.getContext("2d");
     ctx.beginPath();
-
-    if(flag2){
-        i2 = i2 + 1;
-    }
-    else{
-        i2 = i2 - 1 ;
-    }
-    if(i2 == 255 || i2 == 0) {
-        flag2 = !flag2
-        
+    if(time >= 5){
+        ctx.rect(100,200,100,c.height - 200);
     }
 
-    ctx.fillStyle = `rgb(0,${i2},${255 - i2})`;
-    ctx.arc(250, 300, 50, 0, 2 * Math.PI);
-    ctx.fill();
 }
-
-function changeThird() {
-
-    var c = document.getElementById("myCanvas");
-    var ctx = c.getContext("2d");
-    ctx.beginPath();
-
-    
-    ctx.clearRect(0, 0, 200, 800);
-
-   
-    if(flag){
-        i3 = i3 + 10;
-    }
-    else{
-        i3 = i3 - 10 ;
-    }
-    if(i3 == 500 || i3 == 0) {
-        flag = !flag
-    }
-    ctx.fillStyle = `rgb(0,${i2},${255 - i2})`;
-    ctx.arc(100,  50 +  i3, 50, 0, 2 * Math.PI);
-    ctx.fill();
-    
-}
-
-setInterval(changeSecond, 10);
-setInterval(changeFirst, 1000);
-setInterval(changeThird, 10);
