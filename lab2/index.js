77955dcd6eee1a2970d891a2d0c832930f7a968f
Daannko <228776@edu.p.lodz.pl>
2022-10-20 17:16:40 +0200

    added ball that moves in straight line

--- lab2/index.js
+++ lab2/index.js
@@ -3,41 +3,53 @@ var c = document.getElementById("myCanvas");
 c.style.border = "1px solid black";
 var ctx = c.getContext("2d");
 var timer = 0;
+var timerB = 0
 
 
-setInterval(drawS, 1000);
+setInterval(drawS, 100);
+setInterval(drawB,100)
 
 function drawS(){
 
     ctx.beginPath();
 
+    ctx.clearRect(0, 0, 600,800);
+
     if(timer >= 0){
      ctx.rect(0,100,100,c.height - 100);
     }
-    if(timer >= 1){
+    if(timer >= 10){
         ctx.rect(100,200,100,c.height - 200);
     }
-    if(timer >= 2){
+    if(timer >= 20){
         ctx.rect(200,300,100,c.height - 300);
     }
-    if(timer >= 3){
+    if(timer >= 30){
         ctx.rect(300,400,100,c.height - 400);
     }
-    if(timer >= 4){
+    if(timer >= 40){
         ctx.rect(400,500,100,c.height - 500);
     }
 
     ctx.strokeStyle = "black";
     ctx.stroke();
     timer += 1;
-    console.log(timer);
+ 
 }
 
 function drawB(){
 
     ctx.beginPath();
-    if(time >= 5){
-        ctx.rect(100,200,100,c.height - 200);
+    timerB = timerB + 1
+    if(timerB>= 1){
+
+        var x = 50 + timerB * 1;
+        var y = 50 + timerB * 1;
+
+        ctx.arc(x, y, 50, 0, Math.PI*2);
+        ctx.fill();
+        console.log(timer);
+  
     }
 
 }
