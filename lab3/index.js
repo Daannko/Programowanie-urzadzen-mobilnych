aab0487d8fe63ade25f4633c43b6c3bbb04b0d65
Daannko <228776@edu.p.lodz.pl>
2022-10-27 17:34:39 +0200

    bullet can destroy balls

--- lab3/index.js
+++ lab3/index.js
@@ -13,9 +13,8 @@ class Player{
         this.width = 50;
         this.height = 50;
     }
-
     draw = () => {
-        console.log(objects[0].x+" "+ objects[0].y)
+        console.log(player.x+" "+ player.y)
         c.beginPath();
         c.rect(this.x, this.y, 50,50);
         c.fillStyle = "red";
@@ -41,7 +40,27 @@ class Bullet{
       };
 }
 
-var objects = [new Player(0, cvs.height -50 )];
+class Target{
+    constructor(x,y,size){
+        this.x = x;
+        this.y= y;
+        this.size = size;
+        this.active = true;
+    }
+
+    draw = () => {
+        c.beginPath();
+        c.arc(this.x, this.y, this.size, 0, Math.PI*2);
+        c.fillStyle = "green";
+        c.fill();
+        c.stroke();
+      };
+}
+
+
+var player = new Player(0, cvs.height -50 )
+var bullets = [];
+var targers = [];
 
 function check(e) {
 
@@ -49,31 +68,42 @@ function check(e) {
     var code = e.keyCode;
     switch (code) {
         case 37: 
-            if(objects[0].x - 1 >= 0)
-                objects[0].x = objects[0].x - 5;
+            if(player.x - 1 >= 0)
+                player.x = player.x - 5;
             break;
         case 39: 
-            if(objects[0].x + 1 <= cvs.width - 50)
-                objects[0].x = objects[0].x + 5;
+            if(player.x + 1 <= cvs.width - 50)
+                player.x = player.x + 5;
             break;
         default:
-            objects.push(new Bullet(objects[0].x + 25,objects[0].y))
-            console.log(objects)
+            bullets.push(new Bullet(player.x + 25,player.y))
+            targers.push(new Target(player.x + 25, 30 ,20))
     }
 }
 
 function update(){
 
-    objects.forEach(obj => {
-        console.log(obj == Bullet)
-        if(obj instanceof Bullet){
-            obj.y = obj.y - dy;
-        }
+    bullets.forEach(obj => {
+        obj.y = obj.y - dy;
+        targers.forEach(target => {
+            var a = obj.x - target.x;
+            var b = obj.y - target.y;
+            var c = Math.sqrt( a*a + b*b );
+
+            if(c < 10 + target.size)
+                target.active = false;
+        })
     })
 
+
     c.clearRect(0, 0, cvs.width, cvs.height);
-    objects.forEach(obj => {
+    player.draw();
+    bullets.forEach(obj => {
         obj.draw();
+    })
+    targers.forEach(obj => {
+        if(obj.active)
+            obj.draw();
     })
 }
 
